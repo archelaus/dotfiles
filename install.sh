2df2772b3c1ebfae5757368af2de30e148757c29
archelaus <aayush.amoli@protonmail.ch>
2022-09-13 14:43:10 +0530

    update install.sh

--- install.sh
+++ install.sh
@@ -15,13 +15,11 @@ else
 	exit 1
 fi
 
-PATH_DIR="~/.local/bin"
+PATH_DIR="${HOME}/.local/bin"
 [ -d "$PATH_DIR" ] || mkdir -p "$PATH_DIR"
 
 # fish
-sudo apt-add-repository ppa:fish-shell/release-3 &&
-sudo apt update &&
-sudo apt install fish
+which fish >/dev/null || { sudo apt-add-repository ppa:fish-shell/release-3; sudo apt update; sudo apt install fish; }
 
 # cheat.sh
 curl https://cht.sh/:cht.sh > "$PATH_DIR/cht.sh"
@@ -30,11 +28,12 @@ chmod +x "$PATH_DIR/cht.sh"
 # batcat
 sudo apt install bat &&
 ln -s /usr/bin/batcat "$PATH_DIR/bat"
-
+	
 # exa
 curl -s https://api.github.com/repos/ogham/exa/releases/latest | grep browser_download_url | grep linux-$(uname -m)-v | cut -d '"' -f 4 | wget -i- -qO- | busybox unzip - 
 mv ./bin/exa "$PATH_DIR/exa" &&
 rm -rf completions man bin
+chmod +x "$PATH_DIR/exa"
 
 # fd
 sudo apt install fd-find
@@ -44,9 +43,10 @@ sudo apt-get install fzf
 
 # has
 curl -sL https://git.io/_has | tee "$PATH_DIR/has" >/dev/null
+chmod +x "$PATH_DIR/has"
 
 # how2
-npm install -g how2
+which how2 >/dev/null || npm install -g how2
 
 # tldr
 curl -o "$PATH_DIR/tldr" https://raw.githubusercontent.com/raylee/tldr/master/tldr &&
