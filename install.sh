0fe1eb454941d31609fb5a7c868e69d4615e2c27
archelaus <aayush.amoli@protonmail.ch>
2022-11-17 17:11:17 +0530

    update

--- install.sh
+++ install.sh
@@ -10,25 +10,25 @@ if ! command -v systemctl >/dev/null 2>&1; then
     exit 1
 fi
 
-OSARCH=$(uname -m)
-case $OSARCH in 
-    x86_64)
-        BINTAG=Linux_x86_64
-        ;;
-    i*86)
-        BINTAG=Linux_i386
-        ;;
-    arm64)
-        BINTAG=Linux_arm64
-        ;;
-    arm*)
-        BINTAG=Linux_armv6
-        ;;
-    *)
-        echo "unsupported OSARCH: $OSARCH"
-        exit 1
-        ;;
-esac
+# OSARCH=$(uname -m)
+# case $OSARCH in 
+#     x86_64)
+#         BINTAG=Linux_x86_64
+#         ;;
+#     i*86)
+#         BINTAG=Linux_i386
+#         ;;
+#     arm64)
+#         BINTAG=Linux_arm64
+#         ;;
+#     arm*)
+#         BINTAG=Linux_armv6
+#         ;;
+#     *)
+#         echo "unsupported OSARCH: $OSARCH"
+#         exit 1
+#         ;;
+# esac
 
 # if [[ $(id -u) -ne 0 ]]; then
 #    echo "This script must be run as root" 
