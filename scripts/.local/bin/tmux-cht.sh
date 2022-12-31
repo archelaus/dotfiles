3512e9f980d54964d3079c5813cc592da9279f21
archelaus <aayush.amoli@protonmail.ch>
2022-12-31 15:32:14 +0530

    Update tmux-cht.sh

--- scripts/.local/bin/tmux-cht.sh
+++ scripts/.local/bin/tmux-cht.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-selected=$(cat ~/.tmux/tmux-cht-languages ~/.tmux/tmux-cht-command | \
+selected=$(cat ~/.config/tmux/tmux-cht-languages ~/.config/tmux/tmux-cht-command | \
     fzf --bind 'J:preview-down,K:preview-up' --reverse \
     --preview-window wrap)
 
@@ -8,7 +8,7 @@ if [[ -z $selected ]]; then
     exit 0
 fi
 
-if grep -qs "$selected" ~/.tmux/tmux-cht-languages; then
+if grep -qs "$selected" ~/.config/tmux/tmux-cht-languages; then
     read -p "Enter query: " query
     tmux neww bash -c "cht.sh $selected $query | less -r"
 else
