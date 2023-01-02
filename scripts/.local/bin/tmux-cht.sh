d80bf2ffeb5e1b4a566de9c81a788b0d7fe5c879
archelaus <aayush.amoli@protonmail.ch>
2023-01-02 12:24:47 +0530

    Update tmux-cht.sh

--- scripts/.local/bin/tmux-cht.sh
+++ scripts/.local/bin/tmux-cht.sh
@@ -7,9 +7,11 @@ if [[ -z $selected ]]; then
   exit 0
 fi
 
+# tmux neww -n cheat
 if grep -qs "$selected" ~/.config/tmux/tmux-cht-languages; then
   read -p "Enter query: " query
   tmux neww -n cht.sh bash -c "cht.sh $selected $query | less -r"
 else
+  # tmux send-keys -t ':cheat' "cht.sh $selected" C-m
   tmux neww -n cht.sh bash -c "cht.sh $selected | less -r"
 fi
