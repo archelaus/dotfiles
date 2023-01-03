603c93292e759b068c0a028f614ff0a8f5305e8a
archelaus <aayush.amoli@protonmail.ch>
2023-01-03 21:41:05 +0530

    Fixed tmux-cht.sh (finally!)

--- scripts/.local/bin/tmux-cht.sh
+++ scripts/.local/bin/tmux-cht.sh
@@ -3,15 +3,10 @@
 selected=$(cat ~/.config/tmux/tmux-cht-languages ~/.config/tmux/tmux-cht-command |
   fzf-tmux -r)
 
-if [[ -z $selected ]]; then
-  exit 0
-fi
+[[ -z $selected ]] && exit 0
 
-# tmux neww -n cheat
-if grep -qs "$selected" ~/.config/tmux/tmux-cht-languages; then
-  read -p "Enter query: " query
-  tmux neww -n cht.sh bash -c "cht.sh $selected $query | less -r"
+if grep -qs $selected ~/.config/tmux/tmux-cht-languages; then
+  tmux neww -n cht.sh bash -c "read -p 'Enter prompt: ' query; cht.sh $selected \$query | less -r"
 else
-  # tmux send-keys -t ':cheat' "cht.sh $selected" C-m
   tmux neww -n cht.sh bash -c "cht.sh $selected | less -r"
 fi
