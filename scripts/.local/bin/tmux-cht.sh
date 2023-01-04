e79ad1f6ec1996bf04b9785ac34c9cf626844108
archelaus <aayush.amoli@protonmail.ch>
2023-01-04 09:22:35 +0530

    Update tmux stuff

--- scripts/.local/bin/tmux-cht.sh
+++ scripts/.local/bin/tmux-cht.sh
@@ -6,7 +6,7 @@ selected=$(cat ~/.config/tmux/tmux-cht-languages ~/.config/tmux/tmux-cht-command
 [[ -z $selected ]] && exit 0
 
 if grep -qs $selected ~/.config/tmux/tmux-cht-languages; then
-  tmux neww -n cht.sh bash -c "read -p 'Enter prompt: ' query; cht.sh $selected \$query | less -r"
+  tmux neww -n cht.sh bash -c "read -p 'Enter prompt: ' query; clear; cht.sh $selected \$query | less -r"
 else
   tmux neww -n cht.sh bash -c "cht.sh $selected | less -r"
 fi
