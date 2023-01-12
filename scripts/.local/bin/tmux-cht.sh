fd115803da7e0045ce838bdbe5a787ddb6e4395d
archelaus <aayush.amoli@protonmail.ch>
2023-01-12 17:10:39 +0530

    Lots of update to local scripts

--- scripts/.local/bin/tmux-cht.sh
+++ scripts/.local/bin/tmux-cht.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
 selected=$(cat ~/.config/tmux/tmux-cht-languages ~/.config/tmux/tmux-cht-command |
-  fzf-tmux -r)
+  fzf-tmux -p --bind=enter:replace-query+print-query)
 
 [[ -z $selected ]] && exit 0
 
-if grep -FXq $selected ~/.config/tmux/tmux-cht-languages; then
-  tmux neww -n cht.sh bash -c "read -p 'Enter prompt: ' query; clear; cht.sh $selected \$query | less -r"
+if grep -Fxq $selected ~/.config/tmux/tmux-cht-languages; then
+  tmux neww -n cht.sh bash -c "read -p 'Enter prompt: ' query; clear; \
+  curl -s cht.sh/$selected/\${query// /+} | less -r"
 else
-  tmux neww -n cht.sh bash -c "cht.sh $selected | less -r"
+  if grep -Fxq $selected ~/.config/tmux/tmux-cht-command; then
+    tmux neww -n cht.sh bash -c "curl -s cht.sh/$selected | less -r"
+  else
+    echo $selected >> ~/.config/tmux/tmux-cht-command
+    tmux neww -n cht.sh bash -c "curl -s cht.sh/$selected | less -r"
+  fi
 fi
